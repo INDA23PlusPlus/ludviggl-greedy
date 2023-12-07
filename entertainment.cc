0a60e7110ace02b0180c172e3c1a3afa8ed36e73
Ludvig Gunne Lindström <ludvig.gunne.lindstrom@proton.me>
2023-12-07 23:58:38 +0100

    Entertainment Box

---
+++ entertainment.cc
@@ -0,0 +1,42 @@
+
+// Submission ID: 12487834
+
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+int main()
+{
+    int n, k;
+    std::cin >> n >> k;
+
+    std::vector<std::pair<int, int>> activities (n);
+
+    for (auto &[end, start] : activities)
+    {
+        std::cin >> start >> end;
+    }
+
+    std::sort(activities.begin(), activities.end());
+
+    std::multiset<int, std::greater_equal<>> rooms;
+    while (k--) rooms.insert(0);
+
+    int scheduled = 0;
+
+    for (auto [end, start] : activities)
+    {
+        const auto room = rooms.upper_bound(start);
+
+        if (room != rooms.end())
+        {
+            rooms.erase(room);
+            rooms.insert(end);
+            scheduled++;
+        }
+    }
+
+    std::cout << scheduled;
+}
